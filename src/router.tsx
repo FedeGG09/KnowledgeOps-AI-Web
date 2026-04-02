d83f5cfa6b8fa228b7143520e07430b2f11ef9ff
Federico Guillermo Gravina <gravinadavilafederico@gmail.com>
2026-04-02 12:31:06 -0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,66 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./screens/Home/Home";
-import Header from "./components/Header/Header";
-import Upload from "./screens/Upload/Upload";
 import Chat from "./screens/Chat/Chat";
-import LogIn from "./screens/LogIn/LogIn";
-import ForgotPassword from "./screens/ForgotPassword/ForgotPassword";
+import Upload from "./screens/Upload/Upload";
 
-import { Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import VerifyUser from "./screens/SignUp/VerifyUser";
-import ChangePassword from "./screens/ForgotPassword/ChangePassword";
-
-// Componente de Ruta Privada
-function PrivateRoute({ children }: any) {
-  const user = useSelector((state: any) => state.user);
-
-  return user && user.token ? children : <Navigate to="/login" />;
-}
-
-export const router = createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: "/",
-    element: <Header />,
-    children: [
-      { path: "", element: <Home /> },
-      {
-        path: "upload",
-        element: (
-          <PrivateRoute>
-            <Upload />
-          </PrivateRoute>
-        ),
-      },
-      {
-        path: "chat",
-        element: (
-          <PrivateRoute>
-            <Chat />
-          </PrivateRoute>
-        ),
-      },
-    ],
+    element: <Home />,
   },
   {
-    path: "login",
-    element: <LogIn />,
-  },
-
-  {
-    path: "signup",
-    element: <LogIn />, //for safety
+    path: "/chat",
+    element: <Chat />,
   },
   {
-    path: "signup/verify-user",
-    element: <VerifyUser />,
+    path: "/upload",
+    element: <Upload />,
   },
   {
-    path: "forgot-password",
-    element: <ForgotPassword />,
-  },
-  {
-    path: "forgot-password/change-password",
-    element: <ChangePassword />,
+    path: "*",
+    element: <Home />,
   },
 ]);
+
+export default router;
